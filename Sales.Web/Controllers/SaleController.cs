d19006e36f45a7dc1ef6f82dda80f8684eca9a98
DotNet <DotNet>
2017-06-22 19:20:48 +0530

    Add sale.web folder

---
+++ Sales.Web/Controllers/SaleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+
+namespace SalesApp.Controllers
+{
+    using Sales.Data.Interfaces;
+    using SalesApp.Model;
+    using System.Threading.Tasks;
+    [Route("api/[controller]/[action]")]
+    [EnableCors("CorsPolicy")]
+    public class SaleController : Controller
+    {
+        private ISale ISale;
+        public SaleController(ISale _ISale)
+        {
+            this.ISale = _ISale;
+        }
+
+     
+        [HttpPost(Name = "AddSale")]
+        public IActionResult AddSale([FromBody] SaleMaster _saleMaster) {
+                return BadRequest();
+        }
+
+        [HttpGet(Name = "GetSales")]
+        public  IActionResult GetSales()
+        {
+            Task<IEnumerable<SaleMaster>> _TsaleMaster = this.ISale.GetSalesAsync();
+             return new OkObjectResult(Result);
+        }
+        
+    }
+}
